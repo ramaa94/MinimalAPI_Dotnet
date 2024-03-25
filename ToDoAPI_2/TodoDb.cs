83f1166b1c023f141ba6400166d43a67a23b905f
Rama <hermi.ramaa@gmail.com>
2024-03-25 14:39:36 +0100

    Ajoutez des fichiers projet.

---
+++ ToDoAPI_2/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+    class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+
